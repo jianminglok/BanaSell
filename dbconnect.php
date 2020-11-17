bad41f67b0a874010940cc010f5af9d0cd4e1556
Jian Ming Lok <jianminglok@MacBook-Pro.local>
2020-11-17 22:23:54 +0800

    Update for DB

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
 <?php
 //connect to mysql database
-$con = mysqli_connect("localhost", "root", "makerlab", "ogiebooks") or die("Error " . mysqli_error($con));
+$con = mysqli_connect("localhost", "root", "makerlab8899", "ogiebooks") or die("Error " . mysqli_error($con));
 $con->set_charset('utf8');
